cc180680e05824448e526c45b98a88c9203d45cf
Florian Delprat <florian.c.delprat@gmail.com>
2023-05-30 15:26:07 +0200

    Add play button to start node

--- src/rete/node/control/StartNode.ts
+++ src/rete/node/control/StartNode.ts
@@ -1,10 +1,13 @@
+import { ClassicPreset } from 'rete';
 import { Output } from '../../Output';
-import { Node } from '../Node';
+import { Node, controlflowEngine } from '../Node';
+import { ButtonControl } from '../../control/button/button';
 
 export class StartNode extends Node {
 	constructor() {
-		super('Start');
+		super('Start', {height: 130});
 		this.addOutExec();
+		this.addControl('playBtn', new ButtonControl('Play', () => controlflowEngine.execute(this.id)));
 	}
 	execute(_: never, forward: (output: 'exec') => void) {
 		forward('exec');
