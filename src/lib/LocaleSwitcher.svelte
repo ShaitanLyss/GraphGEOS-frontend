81b22847fa1107e25ead8f468461b7bc8397dd8e
MoonLyss <florian.c.delprat@gmail.com>
2023-06-16 16:22:01 +0200

    Add locale switcher

---
+++ src/lib/LocaleSwitcher.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { locale, locales } from 'svelte-i18n';
+
+	// add a property for adding classes to the select
+</script>
+
+<!-- {"rounded-token h-7 cursor-pointer"} -->
+<div>
+	<select class="rounded-token h-7 cursor-pointer text-sm py-0 variant-ringed-surface w-16 pe-0" bind:value={$locale}>
+		{#each $locales as locale}
+			<option value={locale}>{locale}</option>
+		{/each}
+	</select>
+</div>
--- src/localization/i18n.ts
+++ src/localization/i18n.ts
@@ -1,6 +1,7 @@
 import { getLocaleFromNavigator, init, register } from 'svelte-i18n';
 
 register('en', () => import('./locales/en.json'));
+register('fr', () => import('./locales/fr.json'));
 
 await init({
 	fallbackLocale: 'en',
