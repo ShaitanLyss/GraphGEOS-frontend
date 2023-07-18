6ab9eed7f2f5e9a52cbb15e1505fe9738bfc789a
ShaitanLyss <33299808+ShaitanLyss@users.noreply.github.com>
2023-07-18 03:30:03 +0200

    Add python code to reinit solver

--- src/rete/node/makutu/solver/ReinitSolverNode.ts
+++ src/rete/node/makutu/solver/ReinitSolverNode.ts
@@ -1,8 +1,9 @@
-import { NodeFactory } from '../../NodeFactory';
+import type { NodeFactory } from '../../NodeFactory';
 import { SolverAPINode } from './SolverAPINode';
 
 export class ReinitSolverNode extends SolverAPINode {
 	constructor({ factory }: { factory: NodeFactory }) {
 		super({ label: 'Reinit Solver', url: '/reinit-solver', factory });
+		this.pythonComponent.addCode('$(solver).reinitSolver()');
 	}
 }
