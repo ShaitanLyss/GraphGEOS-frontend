54c8787f0b8bbdaadfde51e469d64b580b02ae50
ShaitanLyss <33299808+ShaitanLyss@users.noreply.github.com>
2023-08-03 20:46:33 +0200

    Add selected state to socket

--- src/rete/socket/Socket.ts
+++ src/rete/socket/Socket.ts
@@ -6,16 +6,18 @@ export class Socket extends ClassicPreset.Socket {
 	public readonly isRequired: boolean;
 	public type: SocketType;
 	public value: unknown;
+	public selected: boolean;
 
 	constructor({
 		name = '',
 		isArray = false,
 		isRequired = false,
-		type = 'any'
+		type = 'any',
 	}: { name?: string; isArray?: boolean; isRequired?: boolean; type?: SocketType } = {}) {
 		super(name);
 		this.isArray = isArray;
 		this.isRequired = isRequired;
 		this.type = type;
+		this.selected = false;
 	}
 }
