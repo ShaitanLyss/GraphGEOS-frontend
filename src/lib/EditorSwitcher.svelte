3dc2d0b595f5408be68544a86de19a784a608bad
ShaitanLyss <33299808+ShaitanLyss@users.noreply.github.com>
2023-08-21 03:29:28 +0200

    Position geos heading better

--- src/lib/EditorSwitcher.svelte
+++ src/lib/EditorSwitcher.svelte
@@ -159,7 +159,7 @@
 	<AppShell slotPageContent="relative">
 		<svelte:fragment slot="header">
 			<div class="flex">
-				<h1 class="h3 mx-auto px-2 my-auto pb-1">
+				<h1 class="h3 mx-auto px-2 my-auto pb-1.5">
 					<span class="bg-gradient-to-tr from-red-900 to-yellow-700 dark:from-red-500 dark:to-yellow-500 bg-clip-text text-transparent box-decoration-clone">
 					GEOS
 					</span>
