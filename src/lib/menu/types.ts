808c8bc506e3f31f908ec65c1e9cd7e850a034b0
MoonLyss <shaitan.lyssm@gmail.com>
2023-11-10 15:55:09 +0100

    Add  menu types and utility factory

--- src/lib/menu/types.ts
+++ src/lib/menu/types.ts
@@ -1,3 +1,6 @@
+import type { EditorType } from '$lib/editor/types';
+import type { NodeFactory } from '$rete';
+
 interface IMenuItem {
 	getLabel: () => string;
 	getDescription: () => string;
@@ -10,6 +13,45 @@ enum MenuItemType {
 	Action
 }
 
-// interface INodeMenuItem extends IMenuItem
+interface INodeMenuItem extends IMenuItem {
+	getInTypes: () => string[];
+	getOutTypes: () => string[];
+	getAddNode: ({ factory }: { factory: NodeFactory }) => Node;
+	getEditorType: () => EditorType;
+}
 
-// const a: IMenuItem = { description: 'a', label: 'a' };
+interface IActionMenuItem extends IMenuItem {
+	executeAction: () => void;
+}
+
+function createMenuItem(properties: {
+	label: string;
+	description: string;
+	type: MenuItemType;
+	tags: string[]; // Include tags in the properties
+}): IMenuItem {
+	return {
+		getLabel: () => properties.label,
+		getDescription: () => properties.description,
+		getType: () => properties.type,
+		getTags: () => properties.tags // Implement getter for tags
+	};
+}
+
+function createNodeMenuItem(properties: {
+	label: string;
+	description: string;
+	tags: string[];
+	inTypes: string[];
+	outTypes: string[];
+	addNode: ({ factory }: { factory: NodeFactory }) => Node;
+	editorType: EditorType;
+}): INodeMenuItem {
+	return {
+		...createMenuItem({ ...properties, type: MenuItemType.Node }),
+		getInTypes: () => properties.inTypes,
+		getOutTypes: () => properties.outTypes,
+		getAddNode: ({ factory }) => properties.addNode({ factory }),
+		getEditorType: () => properties.editorType
+	};
+}
