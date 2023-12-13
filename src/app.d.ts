f558c520e479e6b6f36efc0c00b69653be8d8a60
Florian <florian.c.delprat@gmail.com>
2023-12-13 02:34:31 +0100

    Add public config to locals

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,11 +1,14 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
+
+import type { publicConfig } from '$lib/config';
 declare global {
 	namespace App {
 		// interface Error {}
 		interface Locals {
 			getSession: getSession;
 			session: unknown;
+			publicConfig: typeof publicConfig;
 		}
 		// interface PageData {}
 		// interface Platform {}
