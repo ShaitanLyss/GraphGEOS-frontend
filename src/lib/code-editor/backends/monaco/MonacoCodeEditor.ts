70dd62d68688a1232fa74b4f00c7eb36d12e1b33
Florian <florian.c.delprat@gmail.com>
2023-12-18 17:22:57 +0100

    Register base hover provider for geos xml

--- src/lib/code-editor/backends/monaco/MonacoCodeEditor.ts
+++ src/lib/code-editor/backends/monaco/MonacoCodeEditor.ts
@@ -15,6 +15,49 @@ export default class MonacoCodeEditor extends CodeEditor {
 		this.monaco.languages.register({ id: 'geos_xml', extensions: ['.xml'] });
 		this.monaco.languages.setLanguageConfiguration('geos_xml', conf);
 		this.monaco.languages.setMonarchTokensProvider('geos_xml', language);
+		this.monaco.languages.registerHoverProvider('geos_xml', {
+			provideHover(model, position, token) {
+				const hoveredWord = model.getWordAtPosition(position);
+				if (!hoveredWord) return null;
+				console.log('hoveredWord', hoveredWord);
+				// The following regex first discriminates <? to avoid tagging <?xml as a tag
+				const tagMatch = model.findPreviousMatch(
+					'(?:<\\?|>|<(\\w+)[\\s\\r\\n]+|</?\\s*(\\w+))',
+					position,
+					true,
+					false,
+					null,
+					true
+				);
+				const isWithinTag = tagMatch !== null && tagMatch.matches?.at(1) !== undefined;
+				const isTag = tagMatch !== null && tagMatch.matches?.at(2) !== undefined;
+				console.log('isTag', isTag, 'isWithinTag', isWithinTag);
+
+				if (isTag) {
+					return { contents: [{ value: 'Tag : ' + hoveredWord.word }] };
+				}
+				if (isWithinTag) {
+					const attributeMatch = model.findPreviousMatch(
+						'(?:\\w[\\s\\r\\n]*|(=))',
+						{ column: hoveredWord.startColumn, lineNumber: position.lineNumber },
+						true,
+						false,
+						null,
+						true
+					);
+					console.log('attributeMatch', attributeMatch?.matches);
+					console.log(attributeMatch?.matches?.at(1) === undefined);
+					const isAttr =
+						attributeMatch !== null &&
+						attributeMatch.matches?.at(1) === undefined &&
+						attributeMatch.matches?.at(0) !== '=';
+					if (isAttr) {
+						return { contents: [{ value: 'Attribute : ' + hoveredWord.word }] };
+					}
+				}
+				return { contents: [] };
+			}
+		});
 		this.monaco.languages.registerDocumentFormattingEditProvider('geos_xml', {
 			provideDocumentFormattingEdits: (model, options, token) => {
 				const text = model.getValue();
