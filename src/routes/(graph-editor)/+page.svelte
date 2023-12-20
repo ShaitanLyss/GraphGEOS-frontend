3aae66e58c4a60e45c5f92eda1ed0ef13bcaf864
Florian <florian.c.delprat@gmail.com>
2023-12-20 15:22:46 +0100

    Try to fix weird bug

--- src/routes/(graph-editor)/+page.svelte
+++ src/routes/(graph-editor)/+page.svelte
@@ -1,9 +1,5 @@
 <script lang="ts">
 	import { EditorSwitcher } from '$lib/editor';
-	import { setContext } from '$lib/global';
-	import { writable } from 'svelte/store';
-	import type { GeosDataContext } from '$lib/geos';
-	import type { PageData } from './$types';
 </script>
 
 <EditorSwitcher />
