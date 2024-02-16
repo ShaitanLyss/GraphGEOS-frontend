9df7f7bdd7ad6d337aed27cda54a2859bc7dd7f0
Florian <shaitan.lyssm@gmail.com>
2024-02-16 19:54:22 +0100

    Make linux password cookie unsecure

--- src/routes/remote-explorer/password/+server.ts
+++ src/routes/remote-explorer/password/+server.ts
@@ -4,7 +4,9 @@ export const POST: RequestHandler = async ({ request, cookies }) => {
 	const { encrypted_linux_password } = await request.json();
 	cookies.set('encrypted_linux_password', encrypted_linux_password, {
 		httpOnly: true,
-		secure: true,
+		// TODO make secure a config var because it is only fine
+		// for our case since we run in a vpn to access pangea
+		secure: false,
 		maxAge: 60 * 60 * 24 * 64, // 2 months
 		path: '/',
 		sameSite: 'strict'
